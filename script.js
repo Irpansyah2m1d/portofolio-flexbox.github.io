ac6b5a2f4dca95a7811d803815c7e91c52c52667
Irpansyah2m1d <irpansyah810@gmail.com>
2021-08-10 17:39:06 +0700

    buat running text

--- script.js
+++ script.js
@@ -8,28 +8,39 @@ menuToggle.addEventListener("click", function () {
 const body = document.querySelector(".body");
 // body.style.backgroundColor = "red";
 
-const merah = document.querySelector(".merah");
-const hijau = document.querySelector(".hijau");
-const kuning = document.querySelector(".kuning");
-const abuAbu = document.querySelector(".abu-abu");
+// const merah = document.querySelector(".merah");
+// const hijau = document.querySelector(".hijau");
+// const kuning = document.querySelector(".kuning");
+// const abuAbu = document.querySelector(".abu-abu");
 
-merah.addEventListener("click", function () {
-  const body = document.querySelector(".body");
-  body.style.transition = "1s";
-  body.style.backgroundColor = "red";
-});
+// merah.addEventListener("click", function () {
+//   const body = document.querySelector(".body");
+//   body.style.transition = "1s";
+//   body.style.backgroundColor = "red";
+// });
 
-kuning.addEventListener("click", function () {
-  const body = document.querySelector(".body");
-  body.style.backgroundColor = "yellow";
-});
+// kuning.addEventListener("click", function () {
+//   const body = document.querySelector(".body");
+//   body.style.backgroundColor = "yellow";
+// });
 
-hijau.addEventListener("click", function () {
-  const body = document.querySelector(".body");
-  body.style.backgroundColor = "green";
-});
+// hijau.addEventListener("click", function () {
+//   const body = document.querySelector(".body");
+//   body.style.backgroundColor = "green";
+// });
 
-abuAbu.addEventListener("click", function () {
-  const body = document.querySelector(".body");
-  body.style.backgroundColor = "grey";
+// abuAbu.addEventListener("click", function () {
+//   const body = document.querySelector(".body");
+//   body.style.backgroundColor = "grey";
+// });
+
+const warna = document.querySelectorAll(".pilihan-warna div");
+
+warna.forEach(function (war) {
+  war.addEventListener("click", function () {
+    // const pilwar = "red";
+    const pilwarn = war.getAttribute("value");
+    body.style.transition = "1s";
+    body.style.backgroundColor = pilwarn;
+  });
 });
