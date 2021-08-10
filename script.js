c67d35ae38e539bbc942703846208b7e536715de
Irpansyah2m1d <irpansyah810@gmail.com>
2021-08-10 08:48:01 +0700

    fitur ubah warna background

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ const menuToggle = document.querySelector(".menu-toggle input");
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+const body = document.querySelector(".body");
+// body.style.backgroundColor = "red";
+
+const merah = document.querySelector(".merah");
+const hijau = document.querySelector(".hijau");
+const kuning = document.querySelector(".kuning");
+const abuAbu = document.querySelector(".abu-abu");
+
+merah.addEventListener("click", function () {
+  const body = document.querySelector(".body");
+  body.style.transition = "1s";
+  body.style.backgroundColor = "red";
+});
+
+kuning.addEventListener("click", function () {
+  const body = document.querySelector(".body");
+  body.style.backgroundColor = "yellow";
+});
+
+hijau.addEventListener("click", function () {
+  const body = document.querySelector(".body");
+  body.style.backgroundColor = "green";
+});
+
+abuAbu.addEventListener("click", function () {
+  const body = document.querySelector(".body");
+  body.style.backgroundColor = "grey";
+});
